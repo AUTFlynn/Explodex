eeb4a604b8e07b48da3662a4a326d0144f0e97ee
KimiOwenRealon <kimi.realonn@gmail.com>
2025-05-08 02:06:17 +1200

    Pause Button
    Still Janky, whenever you restart or exit to play again, when you click on the first tile, all tiles will disappear

--- explodex/Pause/pause_menu.gd
+++ explodex/Pause/pause_menu.gd
@@ -1,16 +1,20 @@
 class_name PauseMenu
 extends Control
 
-
+func _ready():
+	get_tree().paused = true
 
 func _on_continue_button_down() -> void:
+	get_tree().paused = false
 	queue_free()
 
 
 func _on_restart_button_down() -> void:
-	pass # Replace with function body.
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Scenes/world.tscn")
 
 
 func _on_quit_button_down() -> void:
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Menu/main_menu.tscn")
  
