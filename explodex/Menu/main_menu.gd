e7384650b8bdceaabcc1cfa351452542301f9c00
KimiOwenRealon <kimi.realonn@gmail.com>
2025-06-10 00:04:08 +1200

    Backgrounds
    Added backgrounds to Main Menu, Game and Vicotry screen

--- explodex/Menu/main_menu.gd
+++ explodex/Menu/main_menu.gd
@@ -7,12 +7,20 @@ extends Control
 @onready var options_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/Options_Button as Button
 @onready var options_menu: OptionsMenu = $Options_Menu as OptionsMenu
 @onready var margin_container: MarginContainer = $MarginContainer as MarginContainer
-
+@onready var background_display = $BackgroundDisplay
 
 @onready var start_level = preload("res://Scenes/world.tscn") as PackedScene
 
+
+var background_paths = [
+	preload("res://Background/Background_001.png"),  # Normal
+	preload("res://Background/Background_002.png"),  # Science
+	preload("res://Background/Background_003.png")   # Space
+]
+
 func _ready():
 	handle_connecting_signals()
+	apply_theme_background()
 
 func on_start_pressed() -> void:
 	get_tree().change_scene_to_packed(start_level)
@@ -28,9 +36,22 @@ func on_exit_pressed() -> void:
 func on_exit_options_menu() -> void:
 	margin_container.visible = true
 	options_menu.visible = false
+	
+func _on_theme_changed(index: int) -> void:
+	apply_theme_background()
 
 func handle_connecting_signals() -> void:
 	start_button.button_down.connect(on_start_pressed)
 	options_button.button_down.connect(on_options_pressed)
 	exit_button.button_down.connect(on_exit_pressed)
 	options_menu.exit_options_menu.connect(on_exit_options_menu)
+	options_menu.background_theme_changed.connect(_on_theme_changed)
+	
+func apply_theme_background():
+	var index := 0
+	if FileAccess.file_exists("user://theme.cfg"):
+		var file = FileAccess.open("user://theme.cfg", FileAccess.READ)
+		index = int(file.get_line())
+		file.close()
+
+	background_display.texture = background_paths[index]
--- explodex/Options_Menu/options_menu.gd
+++ explodex/Options_Menu/options_menu.gd
@@ -2,14 +2,27 @@ class_name OptionsMenu
 extends Control
 
 @onready var exit_button: Button = $MarginContainer/VBoxContainer/Exit_Button as Button
+@onready var option_button: OptionButton = $MarginContainer/VBoxContainer/Settings_Tab_Container/TabContainer/Theme/MarginContainer/ScrollContainer/VBoxContainer/Theme_Button/HBoxContainer/OptionButton as Button
 
 signal exit_options_menu
+signal background_theme_changed
 
 func _ready():
 	exit_button.button_down.connect(on_exit_pressed)
 	set_process(false)
+	option_button.connect("item_selected", Callable(self, "_on_theme_selected"))
 
 
 func on_exit_pressed() -> void:
 	exit_options_menu.emit()
 	set_process(false)
+
+
+func _on_theme_selected(index: int):
+	save_theme_choice(index)
+	emit_signal("background_theme_changed", index)
+
+func save_theme_choice(index: int):
+	var file = FileAccess.open("user://theme.cfg", FileAccess.WRITE)
+	file.store_line(str(index))
+	file.close()
